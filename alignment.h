cb31a90bbdd8a0d442eae36ca0e0d42d68b57f24
Austin Clyde <aclyde@uchicago.edu>
2021-08-01 13:43:47 -0500

    inital code

---
+++ alignment.h
@@ -0,0 +1,40 @@
+//
+// Created by Austin Clyde on 8/1/21.
+//
+
+#ifndef LAB3_ALIGNMENT_H
+#define LAB3_ALIGNMENT_H
+
+
+void needleman_wunsch_alignment(const char [][]simMatrix,
+                                const int gapPenalty,
+                                const char *seq1,
+                                const char *seq1,
+                                const int seq1_len,
+                                const char *seq2,
+                                const int seq2_len,
+                                char **align1,
+                                int *align1_len,
+                                char **align2,
+                                int *align2_len
+) {
+    return;
+}
+
+
+void smith_waterman_alignment(const char [][]simMatrix,
+                              const int gapPenalty,
+                              const char *seq1,
+                              const int seq1_len,
+                              const char *seq2,
+                              const int seq2_len,
+                              char **align1,
+                              int *align1_len,
+                              char **align2,
+                              int *align2_len
+) {
+    return;
+}
+
+
+#endif //LAB3_ALIGNMENT_H
